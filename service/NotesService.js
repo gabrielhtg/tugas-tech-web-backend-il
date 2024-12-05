35cf4477862638de03c2b35aa4b666047cfb94a7
ifs21045-itdel <ifs21045@student.del.ac.id>
2024-12-05 08:30:48 +0700

    membuat catatan baru

--- service/NotesService.js
+++ service/NotesService.js
@@ -2,7 +2,36 @@ const pool = require("../config/database");
 
 const getAll = async (req, res) => {};
 
-const create = async (req, res) => {};
+const create = async (req, res) => {
+  const { title, datetime, note } = req.body;
+
+  if (!title || !datetime || !note) {
+    return res.status(400).json({ message: "Semua field wajib diisi!" });
+  }
+
+  try {
+    const [result] = await pool.query(
+      "INSERT INTO notes (title, datetime, note) VALUES (?, ?, ?)",
+      [title, datetime, note]
+    );
+
+    const newNote = {
+      id: result.insertId,
+      title,
+      datetime,
+      note,
+    };
+
+    res.status(201).json(newNote);
+  } catch (error) {
+    console.error("Terjadi kesalahan saat menambahkan catatan:", error);
+    res.status(500).json({ message: "Gagal membuat catatan", error });
+  }
+};
+
+module.exports = {
+  create,
+};
 
 const getById = async (req, res) => {};
 
