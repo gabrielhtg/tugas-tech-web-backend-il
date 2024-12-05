52487e1d12d03010456f621939196ae9c205bbf6
Gerry Bukit <ifs21055@students.del.ac.id>
2024-12-05 09:37:50 +0700

    fix update notes

--- service/NotesService.js
+++ service/NotesService.js
@@ -33,40 +33,32 @@ const update = async (req, res) => {
   const { id } = req.params;
   const { title, datetime, note } = req.body;
 
-  if (!title || !datetime || !note) {
+  if (!id || !title || !datetime || !note) {
     return res.status(400).json({ message: "Semua field wajib diisi!" });
   }
 
   try {
-    const [noteExists] = await pool.query("SELECT * FROM notes WHERE id = ?", [
-      id,
-    ]);
-    if (noteExists.length === 0) {
-      return res.status(404).json({ message: "Catatan tidak ditemukan!" });
-    }
-
     const [result] = await pool.query(
       "UPDATE notes SET title = ?, datetime = ?, note = ? WHERE id = ?",
       [title, datetime, note, id]
     );
 
     if (result.affectedRows === 0) {
-      return res.status(500).json({ message: "Gagal memperbarui catatan" });
+      return res.status(404).json({ message: "Catatan tidak ditemukan" });
     }
 
-    res.status(200).json({
-      message: "Catatan berhasil diperbarui",
-      updatedNote: { id, title, datetime, note },
-    });
-  } catch (error) {
-    console.error("Terjadi kesalahan saat memperbarui catatan:", error);
-    res.status(500).json({ message: "Gagal memperbarui catatan", error });
-  }
-};
+    const updatedNote = {
+      id,
+      title,
+      datetime,
+      note,
+    };
 
-module.exports = {
-  create,
-  update,
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.error("Terjadi kesalahan saat mengupdate catatan:", error);
+    res.status(500).json({ message: "Gagal mengupdate catatan", error });
+  }
 };
 
 const getById = async (req, res) => {};
