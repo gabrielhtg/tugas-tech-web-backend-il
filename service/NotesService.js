70264e609e6b7486546dbb064c1bc4693a6f2082
Gerry Bukit <ifs21055@students.del.ac.id>
2024-12-05 09:32:25 +0700

    edit notes

--- service/NotesService.js
+++ service/NotesService.js
@@ -29,15 +29,49 @@ const create = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { title, datetime, note } = req.body;
+
+  if (!title || !datetime || !note) {
+    return res.status(400).json({ message: "Semua field wajib diisi!" });
+  }
+
+  try {
+    const [noteExists] = await pool.query("SELECT * FROM notes WHERE id = ?", [
+      id,
+    ]);
+    if (noteExists.length === 0) {
+      return res.status(404).json({ message: "Catatan tidak ditemukan!" });
+    }
+
+    const [result] = await pool.query(
+      "UPDATE notes SET title = ?, datetime = ?, note = ? WHERE id = ?",
+      [title, datetime, note, id]
+    );
+
+    if (result.affectedRows === 0) {
+      return res.status(500).json({ message: "Gagal memperbarui catatan" });
+    }
+
+    res.status(200).json({
+      message: "Catatan berhasil diperbarui",
+      updatedNote: { id, title, datetime, note },
+    });
+  } catch (error) {
+    console.error("Terjadi kesalahan saat memperbarui catatan:", error);
+    res.status(500).json({ message: "Gagal memperbarui catatan", error });
+  }
+};
+
 module.exports = {
   create,
+  update,
 };
 
 const getById = async (req, res) => {};
 
 const remove = async (req, res) => {};
-
-const update = async (req, res) => {};
 
 module.exports = {
   getAll,
